822e4c96432848dbefedce80182b9b8c03916797
Fabio Vitalba <fabio.vitalba@fishinggiraffe.com>
2015-11-24 21:45:21 +0100

    V1.10
    - Major Update, should be optimized. Please tell me it works.

--- MarkupConverterTester/Window1.xaml.cs
+++ MarkupConverterTester/Window1.xaml.cs
@@ -1,7 +1,9 @@
 using System.Windows;
 using MarkupConverter;
 using Microsoft.Office.Interop.Excel;
-using System; 
+using System;
+using System.Threading;     // For setting the Localization of the thread to fit
+using System.Globalization; // the of the MS Excel localization, because of the MS bug
 
 namespace MarkupConverter
 {
@@ -86,61 +88,64 @@ namespace MarkupConverter
 
         public void convertXlsRTFtoText(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                var excelApp = new Microsoft.Office.Interop.Excel.Application();
-                excelApp.Workbooks.Open((myTextBox5.Text), Type.Missing, Type.Missing,
-                                                       Type.Missing, Type.Missing,
-                                                       Type.Missing, Type.Missing,
-                                                       Type.Missing, Type.Missing,
-                                                       Type.Missing, Type.Missing,
-                                                       Type.Missing, Type.Missing,
-                                                       Type.Missing, Type.Missing);
-                var ws = excelApp.Worksheets;
-                var worksheet = (Worksheet)ws.get_Item("Sheet1");
+            //try
+            //{
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
 
+            var excelApp = new Microsoft.Office.Interop.Excel.Application();
+            excelApp.Workbooks.Open((myTextBox5.Text), Type.Missing, Type.Missing,
+                                                   Type.Missing, Type.Missing,
+                                                   Type.Missing, Type.Missing,
+                                                   Type.Missing, Type.Missing,
+                                                   Type.Missing, Type.Missing,
+                                                   Type.Missing, Type.Missing,
+                                                   Type.Missing, Type.Missing);
+            var ws = excelApp.Worksheets;
+            var worksheet = (Worksheet)ws.get_Item("Sheet1");
+            System.Windows.Forms.RichTextBox rtBox = new System.Windows.Forms.RichTextBox();
                 object[,] values = new object[(Int32.Parse(upperRow5.Text) - Int32.Parse(lowerRow5.Text) + 1), (Int32.Parse(upperColumn5.Text) - Int32.Parse(lowerColumn5.Text) + 1)];//(object[,])range.Value2;
 
-                for (int row = Int32.Parse(lowerRow5.Text); row <= Int32.Parse(upperRow5.Text); row++)
+            for (int row = Int32.Parse(lowerRow5.Text); row <= Int32.Parse(upperRow5.Text); row++)
+            {
+                for (int column = Int32.Parse(lowerColumn5.Text); column <= Int32.Parse(upperColumn5.Text); column++)
                 {
-                    for (int column = Int32.Parse(lowerColumn5.Text); column <= Int32.Parse(upperColumn5.Text); column++)
+                    string cellName = convertCell(row, column);
+                    string cellVal = Convert.ToString(worksheet.Range[cellName].Value);
+                    try
                     {
-                        string cellName = convertCell(row, column);
-                        string cellVal = Convert.ToString(worksheet.Range[cellName].Value);
-                        try
-                        {
-                            // Convert RTF to Plain Text
-                            if (cellVal.StartsWith(@"{\rtf")) {
-                                System.Windows.Forms.RichTextBox rtBox = new System.Windows.Forms.RichTextBox();
-                                rtBox.Rtf = cellVal;
-                                cellVal = rtBox.Text;
-                                //cellVal.Replace(System.Environment.NewLine, "<<<EOL>>>");
-                                //cellVal.Replace("\n", "<<<EOL>>>");
-                                //cellVal.Replace("\r", "<<<EOL>>>");
-                            }
-                            // Avoid Excel Formula Error
-                            //if (cellVal.Substring(1,1).Equals("="))   {
-                                cellVal = "'" + cellVal;
-                            //}
-
-                            worksheet.Range[cellName].Value = cellVal.Substring(0, cellVal.Length);
+                        // Convert RTF to Plain Text
+                        if (cellVal.StartsWith(@"{\rtf")) {
+                            
+                            rtBox.Rtf = cellVal;
+                            cellVal = rtBox.Text;
+                            
+                            //cellVal.Replace(System.Environment.NewLine, "<<<EOL>>>");
+                            //cellVal.Replace("\n", "<<<EOL>>>");
+                            //cellVal.Replace("\r", "<<<EOL>>>");
                         }
-                        catch (Exception ex2) { }
+                        // Avoid Excel Formula Error
+                        //if (cellVal.Substring(1,1).Equals("="))   {
+                        cellVal = "'" + cellVal;
+                        //}
 
-                        
+                        worksheet.Range[cellName].Value = cellVal.Substring(0, cellVal.Length);
                     }
+                    catch (Exception ex2) { }
                 }
-
-                excelApp.ActiveWorkbook.SaveCopyAs(myTextBox6.Text);
-
-                excelApp.ActiveWorkbook.Close(true);
-                excelApp.Quit();
             }
+
+            excelApp.ActiveWorkbook.SaveCopyAs(myTextBox6.Text);
+
+            excelApp.ActiveWorkbook.Close(true);
+            excelApp.Quit();
+        /*}
+        
             catch (Exception ex)
             {
                 MessageBox.Show("Error while converting: " + ex.ToString());
             }
-
+            */
+            
             MessageBox.Show("Excel Conversion Complete!");
         }
 
